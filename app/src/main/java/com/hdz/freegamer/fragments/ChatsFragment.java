cb61607ecdedbefe898e693141905fe8b1cf7144
Adrian Hernnadez <adrianhdzm13@gmail.com>
2021-11-15 22:13:54 -0500

    Fin mejora pantalla Chats

--- app/src/main/java/com/hdz/freegamer/fragments/ChatsFragment.java
+++ app/src/main/java/com/hdz/freegamer/fragments/ChatsFragment.java
@@ -2,6 +2,8 @@ package com.hdz.freegamer.fragments;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -30,6 +32,8 @@ public class ChatsFragment extends Fragment {
     ChatsProvider mChatsProvider;
     AuthProvider mAuthProvider;
 
+    Toolbar mToolbar;
+
     public ChatsFragment() {
         // Required empty public constructor
     }
@@ -41,11 +45,15 @@ public class ChatsFragment extends Fragment {
         // Inflate the layout for this fragment
         mView = inflater.inflate(R.layout.fragment_chats, container, false);
         mRecyclerView = mView.findViewById(R.id.recyclerViewChats);
+        mToolbar = mView.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(mToolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Chats");
+
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
         mRecyclerView.setLayoutManager(linearLayoutManager);
-
         mChatsProvider = new ChatsProvider();
         mAuthProvider = new AuthProvider();
+
 
         return mView;
     }
